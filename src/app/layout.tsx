fd430ba854972929960d84beca19ed4bbd129421
Raul Santana <ti.raul77@gmail.com>
2023-11-22 22:54:53 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
 import './globals.css'
+import { getCssText } from '@/styles/stitches.config'
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+        <title>BookWise</title>
+      </head>
+      <body className={nunito.className}>{children}</body>
     </html>
   )
 }
